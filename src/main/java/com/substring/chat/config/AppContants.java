b3dc0fefaf702dc39f6524ea8e9d3fd4887c1554
Chavda Ajay <111041866+ajay260902@users.noreply.github.com>
2024-12-13 23:06:56 +0530

    Update AppContants.java

--- src/main/java/com/substring/chat/config/AppContants.java
+++ src/main/java/com/substring/chat/config/AppContants.java
@@ -4,5 +4,5 @@ import org.springframework.stereotype.Component;
 
 @Component
 public  class AppContants {
-  public static final String FRONT_END_URL="http://localhost:5173";
+  public static final String FRONT_END_URL="https://majestic-sprinkles-1df549.netlify.app";
 }
